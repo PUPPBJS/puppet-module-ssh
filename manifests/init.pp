cec56542ea6c2107657e03089dcd670eb87793c2
PUPPBJS <it-aix@bjs.com>
2016-04-08 09:35:48 -0400

    Update init.pp
    added tab at start of line 113 for AIX

--- manifests/init.pp
+++ manifests/init.pp
@@ -110,7 +110,7 @@ class ssh (
       $default_ssh_package_adminfile           = undef
       $default_ssh_sendenv                     = true
       $default_ssh_config_use_roaming          = 'no'
-      $default_sshd_config_subsystem_sftp      = '/usr/sbin/sftp-server'
+      $default_sshd_config_subsystem_sftp      = '\t/usr/sbin/sftp-server'
       $default_sshd_config_mode                = '0600'
       $default_sshd_config_xauth_location      = undef
       $default_sshd_config_use_dns             = 'yes'
